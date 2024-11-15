d522e17933d11ee602159f6734fce4ccfd881199
yamamoto1028 <shuhei.yamamoto.h10@gmail.com>
2024-11-15 15:29:42 +0900

    🎨 about/page.tsxのdummyBooksを切り出した

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,29 +1,8 @@
 "use client";
 import styles from "./about.module.css";
 import { Header } from "../components/Header";
-import { BookToRead } from "../components/BookToRead/BookToRead";
 import BookRow from "../components/BookRow/BookRow";
-
-export const dummyBooks: BookToRead[] = [
-  {
-    id: 1,
-    title: "はじめてのReact",
-    authors: "ダミー",
-    memo: "",
-  },
-  {
-    id: 2,
-    title: "React Hooks入門",
-    authors: "ダミー",
-    memo: "",
-  },
-  {
-    id: 3,
-    title: "実践Reactアプリケーション開発",
-    authors: "ダミー",
-    memo: "",
-  },
-];
+import { dummyBooks } from "../components/dummyBooks/dummyBooks";
 
 export default function About() {
   const bookRows = dummyBooks.map((b) => {
---
+++ src/app/components/dummyBooks/dummyBooks.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { BookToRead } from "../BookToRead/BookToRead";
+
+export const dummyBooks: BookToRead[] = [
+  {
+    id: 1,
+    title: "はじめてのReact",
+    authors: "ダミー",
+    memo: "",
+  },
+  {
+    id: 2,
+    title: "React Hooks入門",
+    authors: "ダミー",
+    memo: "",
+  },
+  {
+    id: 3,
+    title: "実践Reactアプリケーション開発",
+    authors: "ダミー",
+    memo: "",
+  },
+];
